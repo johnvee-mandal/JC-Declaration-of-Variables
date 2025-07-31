08deeb46684b884f479aa40421ab9b5726acbdb2
Johnvee Mandal <johnvee.mandal@GXCHLAP00694.local>
2025-07-31 16:55:45 +0800

    Declaration of Variable Submission

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,49 @@
 package org.example;
-import org.example.packageone.ClassOne;
-import org.example.packagetwo.ClassTwo;
-import org.example.packagethree.ClassThree;
-import static java.lang.System.out;
+import java.lang.String;
 
-// Import here manually
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        ClassOne classOne = new ClassOne();
-        ClassTwo classTwo = new ClassTwo();
-        ClassThree classThree = new ClassThree();
-        out.println(classOne.greet());
-        out.println(classTwo.greet());
-        out.println(classThree.greet());
 
+    // static variables
+    int staticHex = 0x1A2B;
+    static byte staticByte = 0b00000001;
+    static String staticString = "Progress, not perfection";
+
+    // instance variables
+    int instanceBinary = 0b101101;
+    int instanceOctal = 072;
+    double instanceDouble = 1_234.95;
+
+    public static void main(String[] args) {
 
     }
+
+    }
+public void displayInstanceVariables() {
+    System.out.println("\nInstance Variables:");
+    System.out.println(" instanceBinary = " + instanceBinary);
+    System.out.println(" instanceOctal = " + instanceOctal);
+    System.out.println(" instanceDouble = " + instanceDouble);
+}
+
+public void displayStaticVariables() {
+        System.out.println("\nStatic Variables:");
+        System.out.println(" staticHex = " + staticHex);
+        System.out.println(" staticByte = " + staticByte);
+        System.out.println(" staticString = " + staticString);
+    }
+
+public void displayLocalVariables() {
+    int localDecimal = 100;
+    int localHex = 0xFF;
+    int localBinary = 0b1101;
+    int localOctal = 014;
+    int localLong = 10_000_000L;
+    String localString = "Every step I take gets me closer to my goal";
+    System.out.println("LocalVariables:");
+    System.out.println(" localDecimal = " + localDecimal);
+    System.out.println(" localHex = " + localHex);
+    System.out.println(" localBinary = " + localBinary);
+    System.out.println(" localOctal = " + localOctal);
+    System.out.println(" localLong = " + localLong);
+    System.out.println(" localString = " + localString);
 }
